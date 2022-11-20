fb276fe2a13dfcc932c4087b3ee10ea54ade5fd9
daecheol.choi <daecheol.choi@epapyrus.com>
2022-11-20 19:28:53 +0900

    fix: Request Body가 전달되지 않는 오류 수정

--- core/src/main/java/com/grasstudy/user/controller/SessionController.java
+++ core/src/main/java/com/grasstudy/user/controller/SessionController.java
@@ -3,6 +3,7 @@ package com.grasstudy.user.controller;
 import com.grasstudy.user.service.SessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class SessionController {
 	private final SessionService sessionService;
 
 	@RequestMapping(value = "/sign-in", method = RequestMethod.POST)
-	public Mono<ResponseEntity<String>> signIn(Map<String, String> params) {
+	public Mono<ResponseEntity<String>> signIn(@RequestBody Map<String, String> params) {
 		return sessionService.signIn(params.get("email"), params.get("password"))
 				.map(ResponseEntity::ok)
 				.onErrorReturn(ResponseEntity.status(401).build());
--- core/src/test/java/com/grasstudy/user/controller/SessionControllerTest.java
+++ core/src/test/java/com/grasstudy/user/controller/SessionControllerTest.java
@@ -8,12 +8,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(SessionController.class)
@@ -28,14 +29,15 @@ class SessionControllerTest {
 
 	@Test
 	void sign_in() {
-		Mockito.when(sessionService.signIn(any(), any()))
+		Mockito.when(sessionService.signIn(anyString(), anyString()))
 		       .thenReturn(Mono.just(Jwts.builder().compact()));
 
 		webTestClient.post()
 		             .uri("/session/sign-in")
+		             .contentType(MediaType.APPLICATION_JSON)
 		             .body(BodyInserters.fromValue("{\n" +
 				             "  \"email\" : \"mock@mock.com\",\n" +
-				             "  \"password\" : \"12345!@\",\n" +
+				             "  \"password\" : \"12345!@\"\n" +
 				             "}"))
 		             .exchange()
 		             .expectStatus().is2xxSuccessful();
@@ -43,14 +45,15 @@ class SessionControllerTest {
 
 	@Test
 	void sign_in_failure() {
-		Mockito.when(sessionService.signIn(any(), any()))
+		Mockito.when(sessionService.signIn(anyString(), anyString()))
 		       .thenReturn(Mono.error(RuntimeException::new));
 
 		webTestClient.post()
 		             .uri("/session/sign-in")
+		             .contentType(MediaType.APPLICATION_JSON)
 		             .body(BodyInserters.fromValue("{\n" +
 				             "  \"email\" : \"mock@mock.com\",\n" +
-				             "  \"password\" : \"12345!@\",\n" +
+				             "  \"password\" : \"12345!@\"\n" +
 				             "}"))
 		             .exchange()
 		             .expectStatus().isEqualTo(401);
